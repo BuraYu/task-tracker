28192b5f2804bda6681d50facd4f9bad61b8f30b
Burak Yüksel <burak.yueksel@hotmail.com>
2024-01-22 10:31:44 +0100

    add model

---
+++ server/models/taskModel.js
@@ -0,0 +1,23 @@
+import mongoose, { mongo } from "mongoose";
+
+const taskSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    publishYear: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Task = mongoose.model("Tasks", taskSchema);
