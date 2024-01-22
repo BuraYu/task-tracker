587c368257b80f8807249aebd6e7503cff93a54b
Burak Yüksel <burak.yueksel@hotmail.com>
2024-01-22 12:18:14 +0100

    added routes, index,js

---
+++ server/index.js
@@ -0,0 +1,43 @@
+import express from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import taskRoute from "./routes/tasksRoute.js";
+import cors from "cors";
+
+const app = express();
+
+//middleware that alows us to send json data
+app.use(express.json());
+
+//middleware for handling cors policy
+//First option: Allow all origins with default of cors
+
+// app.use(cors());
+
+//allow custom origins
+// app.use(
+//   cors({
+//     origin: "http://localhose:3000",
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     allowedHeaders: ["Content-Type"],
+//   })
+// );
+
+//get ressource from server
+app.get("/", (request, response) => {
+  return response.status(234).send("Welcome to MERN");
+});
+
+app.use("/books", taskRoute);
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("App connected to DB");
+    app.listen(PORT, () => {
+      console.log("App is listening to port", PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
---
+++ server/routes/tasksRoute.js
@@ -0,0 +1,108 @@
+import express from "express";
+import { Task } from "../models/taskModel.js";
+
+const router = express.Router();
+
+// Route for Save a new task
+router.post("/", async (request, response) => {
+  try {
+    if (
+      !request.body.title ||
+      !request.body.author ||
+      !request.body.publishYear
+    ) {
+      return response.status(400).send({
+        message: "Send all required fields: title, author, publishYear",
+      });
+    }
+    const newTask = {
+      title: request.body.title,
+      author: request.body.author,
+      publishYear: request.body.publishYear,
+    };
+
+    const task = await Task.create(newTask);
+
+    return response.status(201).send(task);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+// Route for Get All task from database
+router.get("/", async (request, response) => {
+  try {
+    const tasks = await Task.find({});
+
+    return response.status(200).json({
+      count: tasks.length,
+      data: tasks,
+    });
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+// Route for Get One task from database by id
+router.get("/:id", async (request, response) => {
+  try {
+    const { id } = request.params;
+
+    const task = await Task.findById(id);
+
+    return response.status(200).json(task);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+// Route for Update a task
+router.put("/:id", async (request, response) => {
+  try {
+    if (
+      !request.body.title ||
+      !request.body.author ||
+      !request.body.publishYear
+    ) {
+      return response.status(400).send({
+        message: "Send all required fields: title, author, publishYear",
+      });
+    }
+
+    const { id } = request.params;
+
+    const result = await Task.findByIdAndUpdate(id, request.body);
+
+    if (!result) {
+      return response.status(404).json({ message: "Task not found" });
+    }
+
+    return response.status(200).send({ message: "Task updated successfully" });
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+// Route for Delete a task
+router.delete("/:id", async (request, response) => {
+  try {
+    const { id } = request.params;
+
+    const result = await Task.findByIdAndDelete(id);
+
+    if (!result) {
+      return response.status(404).json({ message: "Task not found" });
+    }
+
+    return response.status(200).send({ message: "Task deleted successfully" });
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+export default router;
