a38e22996ae6bd32d550e9746c8e5965a87e9727
Burak Yüksel <burak.yueksel@hotmail.com>
2024-01-25 09:00:05 +0100

    add style to navbar and header

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -4,8 +4,7 @@ import './Header.css'
 export default function Header() {
   return (
     <div className="header-div-container">
-      <h1 className="header-heading">Test</h1>
-      <a href="#">Sign out</a>
+      <h1 className="header-heading">Task Tracker</h1>
     </div>
   )
 }
