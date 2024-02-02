b70715093ffc832c16990a125448af38626a7793
Burak Yüksel <burak.yueksel@hotmail.com>
2024-02-02 22:07:38 +0100

    add landing page for login

--- client/src/components/LoginButton/LoginButton.js
+++ client/src/components/LoginButton/LoginButton.js
@@ -1,10 +1,19 @@
 import { useAuth0 } from '@auth0/auth0-react'
+import './LoginButton.css'
 
 const LoginButton = () => {
   const { loginWithRedirect, isAuthenticated } = useAuth0()
   return (
     !isAuthenticated && (
-      <button onClick={() => loginWithRedirect()}>Sign In</button>
+      <div className="login-button--container">
+        <h1 className="login-button--main-heading">Task Tracker</h1>
+        <button
+          className="login-button--btn"
+          onClick={() => loginWithRedirect()}
+        >
+          Sign In
+        </button>
+      </div>
     )
   )
 }
